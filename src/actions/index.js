613a30510103e607118eb0eeeb11d41f776c054b
Derek S <dschwan21@me.com>
2019-06-19 14:23:30 -0700

    characters rendered to screen, src/actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,9 @@
+import axios from "axios";
+
+export const FETCH_CHARS_START = "FETCH_CHARS_START";
+export const FETCH_CHARS_SUCCESS = "FETCH_CHARS_SUCCESS";
+export const FETCH_CHARS_ERROR = "FETCH_CHARS_ERROR";
+
 // we'll need axios
 
 // we'll need to create 3 different action types here.
@@ -6,3 +12,14 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+export const getChars = () => dispatch => {
+  dispatch({ type: FETCH_CHARS_START });
+  axios
+    .get(`https://swapi.co/api/people/`)
+    .then(res => {
+      console.log(res.data.results);
+      dispatch({ type: FETCH_CHARS_SUCCESS, payload: res.data.results });
+    })
+    .catch(err => dispatch({ type: FETCH_CHARS_ERROR }));
+};
